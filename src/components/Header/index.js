05ca0fbfa412c7b2811cb5fa79191867d7f3d207
Bruno Souza <brunoalksouza@gmail.com>
2023-02-15 17:37:48 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <span className="title">Controle Financeiro</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
